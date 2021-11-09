81daca9bd1bc91738e2efead93083a1a54774058
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-09 23:41:37 +0600

    test deploy

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { query } = require('express');
+
+const port = process.env.PORT || 5000;
+
+//middlewawire
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server of Assignment 12')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
